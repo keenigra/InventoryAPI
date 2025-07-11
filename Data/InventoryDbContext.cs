2b2b6ec7031bdf40f7e65f7b3e15393ec3d1d515
Kenny Murcia <keenigra@gmail.com>
2025-07-11 14:57:56 -0600

    Agregar archivos de proyecto.

---
+++ Data/InventoryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace InventoryAPI.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
